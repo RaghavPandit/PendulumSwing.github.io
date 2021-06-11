273a6352f3128257bc50875654e7a9b255e62ec5
RaghavPandit <rp20020918@gmail.com>
2021-06-11 10:10:53 +0530

    added variable for damping and mass

--- sketch.js
+++ sketch.js
@@ -1,14 +1,16 @@
 let startX = 200;
-let startY = 50;
+let startY = 200;
 let len = 160;
 let angle = 0;
 let aVelocity = 0;
 let aAcc = 0;
+let damping = 0.995;
+let mass = 0.5;
 
 
 function setup(){
     createCanvas(400,400);
-    angle = -PI/2;
+    angle = PI/2 +PI/4;
     frameRate(60);
 }
 
@@ -23,9 +25,16 @@ function draw(){
     stroke(255);
     strokeWeight(2);
 
-    aAcc = (-9.8 *  sin(angle)); 
+    aAcc = (-9.8*mass *  sin(angle)); 
     aVelocity += aAcc/60;
     angle +=aVelocity/60;
 
-    aVelocity *=.999;
+    aVelocity *=damping;
+
+    print(aVelocity);
+    if(abs(aVelocity) < 0.000008920655905097808){
+        print('Stop');
+        print(bobX,bobY);
+        noLoop();
+    }
 }
